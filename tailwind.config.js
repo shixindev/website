35ca75310e6f6312b7a446fa62b9a4df49f86ecb
Xie <shixinxie2022@gmail.com>
2024-12-09 10:24:44 +0800

    Delete readme file from vite and change require statement to import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import typography from "@tailwindcss/typography"
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -5,5 +7,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [typography],
 }
